90d0ce24ce7828b0b07de796ff5b675dceb9fd0d
MoBinMing <1915809094@qq.com>
2021-01-19 19:12:14 +0800

    启用打包时跳过单元测试

--- src/main/java/com/mobinming/cloud_service/CloudServiceApplication.java
+++ src/main/java/com/mobinming/cloud_service/CloudServiceApplication.java
@@ -3,8 +3,6 @@ package com.mobinming.cloud_service;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @EnableOpenApi
